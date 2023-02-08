06c315c4d4f3cf07e183196420cfd02956c466f7
ganesh070723 <bbabu8342@gmail.com>
2023-02-08 20:59:09 +0530

    q4

--- Todo_project/urls.py
+++ Todo_project/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 
-from django.urls import path, include
+from django.urls import path, include,re_path
+from django.conf import settings
+from django.views.static import serve
 from todo_app.views import TaskList, TaskDetail
 
 urlpatterns = [
      path('admin/', admin.site.urls),
     path('tasks/', TaskList.as_view(), name='task-list'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
